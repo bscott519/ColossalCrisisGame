e4dbd0c94624c9e66543e04e11769dfad75a2c53
bscott519 <113139519+bscott519@users.noreply.github.com>
2024-12-25 19:08:26 -0500

    Combat system start

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var dag_zone = $DamageZone
 
 const SPEED = 200
 const JUMP_VELOCITY = -380.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 900
+
+var attack_type: String
+var cur_attack: bool
+
 
 func _ready():
 	Global.plyrbody = self
+	cur_attack = false
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
@@ -20,16 +24,24 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if !cur_attack:
+		if Input.is_action_just_pressed("left_mouse") or Input.is_action_just_pressed("right_mouse"):
+			cur_attack = true
+			if Input.is_action_just_pressed("left_mouse") and is_on_floor():
+				attack_type = "single"
+			elif Input.is_action_just_pressed("right_mouse") and is_on_floor():
+				attack_type = "double"
+			else:
+				attack_type = "air"
+			handle_attack_anims(attack_type)
+
 	move_and_slide()
-	
 	player_anims(direction)
 
 func player_anims(dir):
@@ -41,6 +53,9 @@ func player_anims(dir):
 			toggle_flip(dir)
 	elif !is_on_floor():
 		animated_sprite_2d.play("jump")
+
+func handle_attack_anims(attack_type):
+	pass
 
 func toggle_flip(dir):
 	if dir == 1:
