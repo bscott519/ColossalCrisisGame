f0d14c131cf319ac51b6446c8865266fba68aebf
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-01-30 19:00:23 -0500

    added flagpole, fixing transition speeds

---
+++ Scripts/scenemanager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var scenes: Dictionary = { "Level 1": "res://Scenes/level_1.tscn", 
+							"Level 2": "res://Scenes/level_2.tscn" }
+
+func transition_to_scene(level : String):
+	var scene_path : String = scenes.get(level)
+	
+	if scene_path != null:
+		#var scene_transition_screen_instance = scene_transition_screen.instantiate()
+		#get_tree().get_root().add_child(scene_transition_screen_instance)
+		await get_tree().create_timer(5.0).timeout
+		get_tree().change_scene_to_file(scene_path)
+		#scene_transition_screen_instance.queue_free()
