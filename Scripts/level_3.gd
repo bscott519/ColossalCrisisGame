908c39f68fef9e9f3075bdfdc55775f9bb8628a4
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-02-04 20:56:16 -0500

    level 3 started and added to level select

---
+++ Scripts/level_3.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var heartsContainer = $CanvasLayer/Hearts
+@onready var player = $player
+@onready var pause_menu = $CanvasLayer2/PauseMenu
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	heartsContainer.setMaxHearts(player.max_health)
+	heartsContainer.updateHearts(player.health)
+	player.healthChanged.connect(heartsContainer.updateHearts)
+	pause_menu.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- Scripts/scenemanager.gd
+++ Scripts/scenemanager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var scenes: Dictionary = { "Level 1": "res://Scenes/level_1.tscn", 
-							"Level 2": "res://Scenes/level_2.tscn" }
+							"Level 2": "res://Scenes/level_2.tscn",
+							"Level 3": "res://Scenes/level_3.tscn" }
 							
 var last_played_scene: String = ""
 
